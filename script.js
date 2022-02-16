324c0d8705b4fe904cb97ffd19701f57890a238e
Jason S. Robinson <json.robinson@gmail.com>
2022-02-16 01:08:15 +0000

    Calculates interest and Rate now displayed when slider changes

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-function compute()
+let compute = () =>
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value
+    var rate = document.getElementById("rate").value
+    var years = document.getElementById("years").value
+    var interest = principal * years * rate / 100
+
+    var year = new Date().getFullYear() + parseInt(years)
 }
-        
+
+let updateRate = () => {
+    var rateval = document.getElementById("rate").value
+    document.getElementById("rate_val").innerText = rateval
+}
